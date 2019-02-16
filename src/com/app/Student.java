f787d58b925f1505811fa140fabb98a8f7b3fccd
ravi <ravi@192.168.100.2>
2019-02-16 15:35:21 +0530

    student data sent successfully

---
+++ src/com/app/Student.java
@@ -0,0 +1,12 @@
+package com.app;
+
+public class Student {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+		System.out.println("done");
+	}
+}
